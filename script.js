183a7b33fe87d05e8800df8e6135b7ebe0f9563f
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-01-12 15:20:22 -0300

    projeto concluido

--- script.js
+++ script.js
@@ -6,12 +6,18 @@ const messageToClarisse = document.querySelector(".clarisse");
 let humanScoreNumber = 0;
 let machineScoreNumber = 0;
 
+const GAME_OPTIONS = {
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors',
+}
+
 const playHuman = (humanChoice) => {
     playTheGame(humanChoice, playMachine())
 }
 
 const playMachine = () => {
-    const choices = ['rock', 'paper', 'scissors']
+    const choices = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
     return choices[Math.floor(Math.random() * 3)]
 }
 
@@ -20,7 +26,7 @@ const playTheGame = (human, machine) => {
     if (human === machine) {
         result.innerHTML = "Empatou!!!"
         messageToClarisse.innerHTML = ``
-    } else if (human === 'rock' && machine === 'scissors' || human === 'paper' && machine === 'rock' || human === 'scissors' && machine === 'paper') {
+    } else if (human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS || human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK || human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER) {
         result.innerHTML = "Você Ganhou!!!"
         messageToClarisse.innerHTML = `<p>Parabéns Clarisse!!!</p>
 
